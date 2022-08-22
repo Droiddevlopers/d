96a0d9ab618fe9c212c3849c6510c517d4a8d33d
Droiddevlopers <droiddevlopers.info@gmail.com>
2022-08-22 16:55:08 +0530

    Initial commit

---
+++ Toast/src/main/java/skinsbox/fffskinsbundle/fff/skintool/fffskinleaders/fffskintool/emotes/toast/Toaster.java
@@ -0,0 +1,11 @@
+package skinsbox.fffskinsbundle.fff.skintool.fffskinleaders.fffskintool.emotes.toast;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Toaster {
+
+    public static void s(Context context,String s){
+        Toast.makeText(context, s, Toast.LENGTH_SHORT).show();
+    }
+}
