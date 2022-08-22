f4dd895f4cf79e9447b1efe9aa433495f6c0fed9
Droiddevlopers <droiddevlopers.info@gmail.com>
2022-08-22 17:39:38 +0530

    okxxxx

--- Toast/src/main/java/skinsbox/fffskinsbundle/fff/skintool/fffskinleaders/fffskintool/emotes/toast/Toaster.java
+++ Toast/src/main/java/skinsbox/fffskinsbundle/fff/skintool/fffskinleaders/fffskintool/emotes/toast/Toaster.java
@@ -11,4 +11,7 @@ public class Toaster {
     public static void cc(Context context,String s){
         Toast.makeText(context, s, Toast.LENGTH_SHORT).show();
     }
+    public static void c(Context context,String s){
+        Toast.makeText(context, s, Toast.LENGTH_SHORT).show();
+    }
 }
